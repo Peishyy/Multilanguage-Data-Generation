8f3eda76594213e3f58f42b78c3b18407c92bc9b
George Stephen <georgesteve561@gmail.com>
2023-09-30 10:10:54 +0300

    testing the excel generator

--- Functions/generate_excel.py
+++ Functions/generate_excel.py
@@ -18,18 +18,22 @@ def generate_excel_files_for_language(folder_path):
                     if language == 'en-US':
                         language_id = data.get('id')
                         utt = data.get('utt')
+                        scenario = data.get('scenario')
+                        intent = data.get('intent')
                         annot_utt = data.get('annot_utt')
 
                         if language_id not in language_data:
-                            language_data[language_id] = {'id': [], 'utt': [], 'annot_utt': []}
+                            language_data[language_id] = {'id': [], 'utt': [],'scenario': [], 'intent': [],'annot_utt': []}
 
                         language_data[language_id]['id'].append(language_id)
                         language_data[language_id]['utt'].append(utt)
+                        language_data[language_id]['scenario'].append(scenario)
+                        language_data[language_id]['intent'].append(intent)
                         language_data[language_id]['annot_utt'].append(annot_utt)
 
     # Create Excel files for each language
     for language_id, data_dict in language_data.items():
         df = pd.DataFrame(data_dict)
-        excel_filename = os.path.join(folder_path, f'{language_id}.xlsx')
+        excel_filename = os.path.join(folder_path+ "/output", f'en-{language_id}.xlsx')
         df.to_excel(excel_filename, index=False)
         print(f'Excel file {excel_filename} created for language {language_id}')
