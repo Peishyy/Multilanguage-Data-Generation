baeb176686c4ca2129f07566fc73fc9c7acdc351
George Stephen <georgesteve561@gmail.com>
2023-10-02 12:04:50 +0300

    final adjustments

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from Functions.process_json import process_jsonl_files
 
 
 def main():
+
+    # This is the function of question 2
     input_folder = 'data'
     output_folder = 'data/output'
     languages_of_interest = ['en-US', 'sw-KE', 'de-DE']
     process_jsonl_files(input_folder, output_folder, languages_of_interest)
+
+    # This is the function of question 1
     #folder_path = "data"
     #generate_excel_files_for_language(folder_path)
-#generate execl files for language
+
+
 if __name__ == "__main__":
     main()
 
